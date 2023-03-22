41152984e6ec6a999807becec0a242340afbf069
Dominic Ayre <domayre@outlook.com>
2023-03-22 10:47:34 +0000

    Add code to build a container with a user specified payload

---
+++ infra/images.py
@@ -0,0 +1,72 @@
+import argparse
+import io
+import os
+import docker
+
+# Set up the Docker client
+client = docker.from_env()
+
+
+def build_docker_image(payload: str, tag: str) -> docker.models.images.Image:
+    # Build the Docker image with the file path mounted onto it
+    image, build_logs = client.images.build(
+        dockerfile="infra/Dockerfile",
+        tag=tag,
+        path=os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
+        buildargs={"payload": payload},
+    )
+
+    # Print the logs from the build process
+    for line in build_logs:
+        print(line.get("stream", "").strip())
+
+    # Print the ID of the new image
+    print(f"Built new image with ID {image.id}")
+
+    # Return the Docker image object
+    return image
+
+
+def publish_docker_image(
+    image: docker.models.images.Image, registry: str, repository: str, tag: str
+) -> None:
+    # Authenticate with the container registry
+    client.login(registry=registry)
+
+    # Tag the Docker image with the repository and tag
+    image.tag(repository=repository, tag=tag)
+
+    # Push the Docker image to the container registry
+    push_logs = client.images.push(repository=repository, tag=tag)
+
+    # Print the logs from the push process
+    for line in push_logs:
+        print(line.get("status", "").strip())
+
+
+if __name__ == "__main__":
+    # Define the command-line arguments
+    parser = argparse.ArgumentParser(
+        description="Build and publish a Docker image with a file mounted onto it"
+    )
+    parser.add_argument("file_path", help="The path to the file to be mounted")
+    parser.add_argument(
+        "--tag", help="The tag to use for the Docker image", required=True
+    )
+    parser.add_argument(
+        "--registry", help="The container registry to publish the image to"
+    )
+    parser.add_argument("--repository", help="The repository name for the image")
+    parser.add_argument(
+        "--push-tag", default="latest", help="The tag for the published image"
+    )
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Build the Docker image with the provided file path
+    image = build_docker_image(args.file_path, tag=args.tag)
+
+    # If a container registry is specified, publish the Docker image to that registry
+    if args.registry and args.repository:
+        publish_docker_image(image, args.registry, args.repository, tag=args.push_tag)

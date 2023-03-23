d594a03c85aa9c227dbae00ea77ede78904910fc
Dominic Ayre <domayre@outlook.com>
2023-03-23 13:33:54 +0000

    Format payload

--- tests/simple_server/simple_server_payload.py
+++ tests/simple_server/simple_server_payload.py
@@ -3,33 +3,36 @@
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-ENDPOINTS={
-    '/get_attestation': {
-        'method': 'GET',
-        'response': {
-            'status': 200,
-            'content_type': 'text/plain',
-            'body': f'Getting attestation{os.linesep}',
+ENDPOINTS = {
+    "/get_attestation": {
+        "method": "GET",
+        "response": {
+            "status": 200,
+            "content_type": "text/plain",
+            "body": f"Getting attestation{os.linesep}",
         },
     }
 }
 
+
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path in ENDPOINTS and ENDPOINTS[self.path]['method'] == 'GET':
+        if self.path in ENDPOINTS and ENDPOINTS[self.path]["method"] == "GET":
             self.send_response(200)
-            response = ENDPOINTS[self.path]['response']
-            self.send_header('Content-type', response['content_type'])
+            response = ENDPOINTS[self.path]["response"]
+            self.send_header("Content-type", response["content_type"])
             self.end_headers()
-            self.wfile.write(response['body'].encode('utf-8'))
+            self.wfile.write(response["body"].encode("utf-8"))
         else:
             self.send_response(404)
             self.end_headers()
 
+
 def run():
-    httpd = HTTPServer(('', 8000), MyRequestHandler)
-    print('Server started...')
+    httpd = HTTPServer(("", 8000), MyRequestHandler)
+    print("Server started...")
     httpd.serve_forever()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()

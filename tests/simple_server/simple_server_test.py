4fa46be5f05f8bcf951c3e63c7ba8fe9aba7f9a4
Dominic Ayre <domayre@outlook.com>
2023-04-17 11:07:16 +0000

    Increase max retries

--- tests/simple_server/simple_server_test.py
+++ tests/simple_server/simple_server_test.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import requests
 import os
+from requests.adapters import HTTPAdapter
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
@@ -10,8 +11,10 @@ from infra.aci_test_case import AciTestCase
 
 class SimpleServerTest(AciTestCase):
     def test_get_attestation(self):
+        session = requests.Session()
+        session.mount("http://", HTTPAdapter(max_retries=10))
         assert self.container_ip is not None
-        response = requests.get(f"http://{self.container_ip}:8000/get_attestation")
+        response = session.get(f"http://{self.container_ip}:8000/get_attestation")
         assert response.status_code == 200
         assert response.content == b"Getting attestation\n"
 

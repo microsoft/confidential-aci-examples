c06e472e0a65a087c4d20f8d75e6f63bc4e7d070
Dominic Ayre <domayre@outlook.com>
2023-04-25 13:07:56 +0000

    Increase retries for the sidecar example

--- tests/simple_sidecar/simple_sidecar_test.py
+++ tests/simple_sidecar/simple_sidecar_test.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import requests
 import os
+from requests.adapters import HTTPAdapter
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
@@ -11,7 +12,9 @@ from infra.aci_test_case import AciTestCase
 class SimpleSidecarTest(AciTestCase):
     def test_sidecar_connection(self):
         assert self.container_ip is not None
-        response = requests.get(f"http://{self.container_ip}:8000/check_connection")
+        session = requests.Session()
+        session.mount("http://", HTTPAdapter(max_retries=10))
+        response = session.get(f"http://{self.container_ip}:8000/check_connection")
         assert response.status_code == 200
         assert response.content == b"True"
 

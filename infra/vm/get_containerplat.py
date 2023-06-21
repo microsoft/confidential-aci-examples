b2b15acb429000755e9d3f58cf734ab5007ec594
Dominic Ayre <domayre@outlook.com>
2023-06-21 11:17:11 +0000

    Run in SNP mode

--- infra/vm/get_containerplat.py
+++ infra/vm/get_containerplat.py
@@ -4,7 +4,7 @@ import json
 
 def get_containerplat(directory_path: str):
     subprocess.run(
-        f'az artifacts universal download --organization "https://dev.azure.com/msazure/" --project "dcf1de98-e135-4121-8a6c-99b73705f581" --scope project --feed "ContainerPlat-Dev" --name "containerplat-confidential-aci" --version "0.1.0-rc.3" --path "{directory_path}"',
+        f'az artifacts universal download --organization "https://dev.azure.com/msazure/" --project "dcf1de98-e135-4121-8a6c-99b73705f581" --scope project --feed "ContainerPlat-Prod" --name "containerplat-confidential-aci" --version "0.1.3" --path "{directory_path}"',
         shell=True,
     )
 

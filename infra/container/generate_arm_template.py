be9064a3976efa1e0ca8a94472ff6781aea62d55
Dominic Ayre <domayre@outlook.com>
2023-06-28 12:54:41 +0000

    Make container name a variable for simpler reference

--- infra/container/generate_arm_template.py
+++ infra/container/generate_arm_template.py
@@ -21,7 +21,9 @@ def generate_arm_template(
         "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
         "contentVersion": "1.0.0.0",
         "parameters": {},
-        "variables": {},
+        "variables": {
+            "uniqueId": name,
+        },
         "resources": [
             {
                 "type": "Microsoft.ContainerInstance/containerGroups",

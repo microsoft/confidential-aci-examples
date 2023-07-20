75cb0a057d5bfc7ae85cf009746d3f4481657941
Heather Garvison <hgarvison@yahoo.com>
2023-07-20 16:48:07 +0000

    adding more tests

--- examples/encrypted_filesystem/payload.py
+++ examples/encrypted_filesystem/payload.py
@@ -1,21 +1,86 @@
+import os
+import requests
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-import requests
+MOUNT_DIR = "/mnt/remote/share"
+LOG_FILE = "~/log.txt"
+KEY_NAME = os.environ["UNIQUE_ID"] + "-key"
+
+def read_file(path: str) -> str:
+    with open(path, "r") as f:
+        return f.read()
+
+def write_file(path: str, content: str) -> str:
+    try:
+        # make directory if it doesn't exist
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        # make file if it doesn't exist
+        with open(path, "w") as f:
+            f.write(content)
+        return read_file(path)
+    except Exception as e:
+        return "Failed to write file"
+
+def scan_file(path: str, text: str) -> str:
+    try:
+        with open(path, "r") as f:
+            if text in f.read():
+                return "Found text in file"
+            else:
+                return "Did not find text in file"
+    except Exception as e:
+        return "Failed to scan file"
+
+ENDPOINTS = {
+    "/read": {
+        "method": "GET",
+        "response": {
+            "content_type": "text/plain",
+            "body": read_file(os.path.join(MOUNT_DIR, "file.txt")),
+        },
+    },
+    "/write": {
+        "method": "GET",
+        "response": {
+            "content_type": "text/plain",
+            "body": write_file(os.path.join(MOUNT_DIR, "new_file.txt"), "This is a new file in the encrypted filesystem!")
+        },
+    },
+    "/maa_token": {
+        "method": "GET",
+        "response": {
+            "content_type": "text/plain",
+            "body": scan_file(LOG_FILE, "retrieving MAA token from MAA endpoint failed")
+        },
+    },
+    "/snp_report": {
+        "method": "GET",
+        "response": {
+            "content_type": "text/plain",
+            "body": scan_file(LOG_FILE, "fetching snp report failed")
+        },
+    },
+    "/key": {
+        "method": "GET",
+        "response": {
+            "content_type": "text/plain",
+            "body": scan_file(LOG_FILE, f"releasing the key {KEY_NAME} failed.")
+        },
+    }
+}
 
 
 class MyRequestHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        response = requests.post(
-            url=f"http://localhost:8080{self.path}",
-            headers=dict(self.headers),
-            data=self.rfile.read(int(self.headers.get("Content-Length", 0))),
-        )
-        self.send_response(response.status_code)
-        for header, value in response.headers.items():
-            self.send_header(header, value)
-        self.end_headers()
-
-        self.wfile.write(response.content)
+    def do_GET(self):
+        if self.path in ENDPOINTS and ENDPOINTS[self.path]["method"] == "GET":
+            self.send_response(200)
+            response = ENDPOINTS[self.path]["response"]
+            self.send_header("Content-type", response["content_type"])
+            self.end_headers()
+            self.wfile.write(response["body"].encode("utf-8"))
+        else:
+            self.send_response(404)
+            self.end_headers()
 
 
 def run():
--- examples/encrypted_filesystem/test.py
+++ examples/encrypted_filesystem/test.py
@@ -4,6 +4,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
+from infra.http_request import request
 from infra.test_case import TestCase
 
 
@@ -11,7 +12,40 @@ class EncryptedFilesystemTest(TestCase):
     def test_encfs_deployment_succeeded(self):
         assert self.container_ip is not None
 
-    # To-Do: Add more/better tests here
+    def test_encfs_read(self):
+        response = request(f"http://{self.container_ip}:8000/read")
+        assert response.status_code == 200
+        assert response.content.decode("utf-8").strip("\n") == "This is a file in the encrypted filesystem!"
+
+    # can't do the write test until the public encfs image is updated
+    # def test_encfs_write(self):
+    #     assert self.container_ip is not None
+
+    #     response = request(f"http://{self.container_ip}:8000/write")
+    #     assert response.status_code == 200
+    #     assert response.content.decode("utf-8").strip("\n") == "This is a new file in the encrypted filesystem!"
+
+    def test_maa_token_acquired(self):
+        response = request(f"http://{self.container_ip}:8000/maa_token")
+        assert response.status_code == 200
+        # searches that 'retrieving MAA token from MAA endpoint failed' not in log file
+        # can search that 'MAA Token:' in log file instead
+        assert response.content.decode("utf-8").strip("\n") == "Did not find text in file"
+
+
+    def test_snp_report_acquired(self):
+        response = request(f"http://{self.container_ip}:8000/snp_report")
+        assert response.status_code == 200
+        # searches that 'fetching snp report failed' not in log file
+        # can search that 'SNPReportBytes:' in log file instead
+        assert response.content.decode("utf-8").strip("\n") == "Did not find text in file"
+
+    def test_key_acquired(self):
+        response = request(f"http://{self.container_ip}:8000/key")
+        assert response.status_code == 200
+        # searches that 'releasing the key {KEY_NAME} failed.' not in log file
+        # can search that 'Key Type: oct-HSM Key' in log file instead
+        assert response.content.decode("utf-8").strip("\n") == "Did not find text in file"
 
 
 if __name__ == "__main__":

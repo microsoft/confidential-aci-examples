6fd243dc16ffbcba215dd365f3bf00a5a9d81836
Dominic Ayre <domayre@outlook.com>
2023-07-21 15:54:32 +0000

    Fix unittest discover command

--- infra/test_case.py
+++ infra/test_case.py
@@ -2,6 +2,7 @@ import json
 import os
 import unittest
 import uuid
+import inspect
 
 from infra.container.test_case import setUpAci, tearDownAci
 from infra.vm.test_case import setUpVm, tearDownVm
@@ -11,7 +12,7 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         self.container_ip = None
 
-        self.test_name = self.__class__.__module__.split(".")[0]
+        self.test_name = inspect.getfile(self.__class__).split("/")[-2]
         self.image_tag = str(uuid.uuid4())
         self.name = str(uuid.uuid4())
 

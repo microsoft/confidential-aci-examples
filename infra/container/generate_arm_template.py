f4273c8fbff32012c042c01ad2db9d7eeaae9014
Dominic Ayre <domayre@outlook.com>
2023-07-25 09:08:51 +0000

    .

--- infra/container/generate_arm_template.py
+++ infra/container/generate_arm_template.py
@@ -18,6 +18,10 @@ def generate_arm_template(
     security_policy: Optional[str] = None,
     out: Optional[str] = None,
 ):
+    if "UNIQUE_ID" not in os.environ:
+        os.environ["UNIQUE_ID"] = name
+    manifest = resolve_manifest_variables(manifest)
+
     print(f"Generating ARM template for {name}")
     arm_template = {
         "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",

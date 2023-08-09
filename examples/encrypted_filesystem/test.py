7cc150db780aaaae636af818f11adafef877b309
Heather Garvison <hgarvison@yahoo.com>
2023-08-09 15:13:35 +0000

    uncommenting write test

--- examples/encrypted_filesystem/test.py
+++ examples/encrypted_filesystem/test.py
@@ -18,13 +18,12 @@ class EncryptedFilesystemTest(TestCase):
             == "This is a file in the encrypted filesystem!"
         )
 
-    # can't do the write test until the public encfs image is updated
-    # def test_encfs_write(self):
-    #     assert self.container_ip is not None
+    def test_encfs_write(self):
+        assert self.container_ip is not None
 
-    #     response = request(f"http://{self.container_ip}:8000/write")
-    #     assert response.status_code == 200
-    #     assert response.content.decode("utf-8").strip("\n") == "This is a new file in the encrypted filesystem!"
+        response = request(f"http://{self.container_ip}:8000/write")
+        assert response.status_code == 200
+        assert response.content.decode("utf-8").strip("\n") == "This is a new file in the encrypted filesystem!"
 
 
 if __name__ == "__main__":

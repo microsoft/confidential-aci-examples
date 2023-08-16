02f5be08c0cfd4666699bf2734b357a463704a99
Dominic Ayre <domayre@outlook.com>
2023-08-16 08:25:23 +0000

    Make HCSShim url configurable

--- infra/vm/get_containerplat.py
+++ infra/vm/get_containerplat.py
@@ -5,18 +5,19 @@ import shutil
 import subprocess
 import json
 import tempfile
+from typing import Optional
 import zipfile
 
 
 def build_hcsshim(
     package_path: str,
-    hcsshim_path: str = "git@github.com:microsoft/hcsshim.git",
+    hcsshim_url: str,
 ):
     with tempfile.TemporaryDirectory() as hcsshim_dir:
         with update_package(package_path) as package_dir:
             os.chdir(hcsshim_dir)
             subprocess.check_output(
-                f"git clone {hcsshim_path} github.com/Microsoft/hcsshim", shell=True
+                f"git clone {hcsshim_url} github.com/Microsoft/hcsshim", shell=True
             )
             os.chdir("github.com/Microsoft/hcsshim")
             os.environ["GOPATH"] = hcsshim_dir
@@ -51,7 +52,10 @@ def update_package(package_path: str):
                     zip_ref.write(file_path, os.path.relpath(file_path, unzipped_dir))
 
 
-def get_containerplat(directory_path: str):
+def get_containerplat(
+    directory_path: str,
+    hcsshim_url: Optional[str] = None,
+):
     subprocess.run(
         f'az artifacts universal download --organization "https://dev.azure.com/msazure/" --project "dcf1de98-e135-4121-8a6c-99b73705f581" --scope project --feed "ContainerPlat-Prod" --name "containerplat-confidential-aci" --version "0.1.3" --path "{directory_path}"',
         shell=True,
@@ -73,6 +77,8 @@ def get_containerplat(directory_path: str):
         shell=True,
     )
 
-    build_hcsshim(
-        package_path=f"{directory_path}/package.zip",
-    )
+    if hcsshim_url is not None:
+        build_hcsshim(
+            package_path=f"{directory_path}/package.zip",
+            hcsshim_url=hcsshim_url,
+        )

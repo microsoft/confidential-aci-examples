ad872754532700bb034d55d57ef86e66b86c0c5c
Dominic Ayre <domayre@outlook.com>
2023-08-18 09:47:01 +0000

    Build userspace so that GCS matches HCSShim

--- infra/vm/build_hcsshim.py
+++ infra/vm/build_hcsshim.py
@@ -22,6 +22,8 @@ def build_hcsshim(
                 f"git clone {hcsshim_url} github.com/Microsoft/hcsshim", shell=True
             )
             os.chdir("github.com/Microsoft/hcsshim")
+
+            # Build Go targets
             os.environ["GOPATH"] = hcsshim_dir
             os.environ["GOOS"] = "windows"
             os.environ["GO_BUILD_FLAGS"] = "-tags=rego"
@@ -40,4 +42,19 @@ def build_hcsshim(
                 )
             for file in glob.glob("*.exe"):
                 shutil.copy(file, package_dir)
+
+            # Build Userspace
+            kernel_dir = "linux_kernel"
+            os.mkdir(kernel_dir)
+            subprocess.run(
+                f'az artifacts universal download --organization "https://dev.azure.com/msazure/" --project "dcf1de98-e135-4121-8a6c-99b73705f581" --scope project --feed "ContainerPlat-Dev" --name "enlightened-linux" --version "0.1.5" --path {kernel_dir}',
+                shell=True,
+            )
+            shutil.copy(
+                f"{kernel_dir}/core-image-minimal-aci-rootfs.tar", "base.tar.gz"
+            )
+            subprocess.run("make all && make rootfs", shell=True)
+            shutil.copy("out/initrd.img", f"{package_dir}/LinuxBootFiles/initrd.img")
+            shutil.copy("out/rootfs.vhd", f"{package_dir}/LinuxBootFiles/rootfs.vhd")
+
     prev_dir = os.chdir(prev_dir)

ce2642e29e2d033b3d0016767a5f4ddb39aaaa98
Heather Garvison <hgarvison@yahoo.com>
2023-11-07 20:29:25 +0000

    initial commit of skr grpc tests

--- examples/key_release/test.py
+++ examples/key_release/test.py
@@ -123,5 +123,29 @@ class KeyReleaseTest(TestCase):
         assert json.loads(response.content.decode())["token"] != ""
 
 
+class KeyReleaseGRPCTest(TestCase):
+    def test_grpc_interface_ready(self):
+        assert self.container_ip is not None
+
+        response = request(f"http://{self.container_ip}:8000/grpc_ready")
+
+        assert response.status_code == 200, response.content.decode("utf-8")
+        assert (
+            response.content.decode("utf-8").strip("\n")
+            == "This is a GRPC interface test"
+        )
+
+    # def test_grpc_interface_ready(self):
+    #     assert self.container_ip is not None
+
+    #     response = request(f"http://{self.container_ip}:8000/grpc_snp_report")
+
+    #     assert response.status_code == 200, response.content.decode("utf-8")
+    #     assert (
+    #         response.content.decode("utf-8").strip("\n")
+    #         == "\"reportHexString\": \"0"
+    #     )
+
+
 if __name__ == "__main__":
     unittest.main()

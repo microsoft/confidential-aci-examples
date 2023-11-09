5f479b531adca5b6ad4c55faf8184a45559deb51
Heather Garvison <hgarvison@yahoo.com>
2023-11-09 21:21:19 +0000

    add debug msg

--- examples/key_release/test.py
+++ examples/key_release/test.py
@@ -130,6 +130,7 @@ class KeyReleaseGRPCTest(TestCase):
         response = request(f"http://{self.container_ip}:8000/grpc_ready")
 
         assert response.status_code == 200, response.content.decode("utf-8")
+        print(response.content.decode("utf-8"))
         assert (
             "Hello GRPC interface test" in response.content.decode("utf-8").strip("\n")
         )
@@ -140,6 +141,7 @@ class KeyReleaseGRPCTest(TestCase):
         response = request(f"http://{self.container_ip}:8000/grpc_snp_report")
 
         assert response.status_code == 200, response.content.decode("utf-8")
+        print(response.content.decode("utf-8"))
         assert (
             "\"reportHexString\": \"0" in response.content.decode("utf-8").strip("\n")
         )

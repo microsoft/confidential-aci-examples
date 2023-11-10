dd4f96ad5483814a2f7eca123bae06b7e1ee38b9
Heather Garvison <hgarvison@yahoo.com>
2023-11-10 16:17:30 +0000

    updating wrapped

--- examples/key_release/payload.py
+++ examples/key_release/payload.py
@@ -10,7 +10,7 @@ import requests
 def create_grpc_test_files():
     if not os.path.isfile("wrapped"):
         with open("wrapped", "w") as f:
-            f.write("eyJraWQiOiJ0ZXN0a2V5MDAwIiwid3JhcHBlZF9kYXRhIjoiSVZuYzM2Smg3SXJUMkpzSkszaG0vNFdHcVdDblJtL0RteHNiWDZoTXJNc0RCbm5SWGhEbFAySElZMEpxejFxMUhMdjFFUHQ3cUg0Z0ZKN3VhOTdhZERjZnRrdDJJNlUrbnVIeGtWZnJLV1IxcnVBTUhzVTBJYVFsM1hUb1JUTzlEYzAvRHdXdVRZWUJCcUY5UDBlcXI0bVk0b0MrTzhXVWlrWjVBd01pcE5BMWFCWGZTNFc1dEtwS0YxVEhzcHpWNkNsamhic2NqaWN2UUxPT3FBdHNJUkpHZTJCN0hwTzBrdEtVRUxVZXBZcHNtR0NPRUZxQnBENkRsd2xqZXB0RW5GaXlvcmNqKzJDODVFMFQzcDNFRllEUktsMTNxWFVHaWRnL1k5bnNZdlJhSXdWWlQ3WTN2dEdVZy9jT0RzSFovS0Q5aXFxalJWVitiOWpTVEZMRmFyY0UybXpwTlFBT2lXL3FBSnB3TFlObmpFSzNkQzI2bEN5a1h1bkZvbjlGWVVHZi9rMEZUb0hGT0Qyd05XcWNSS0hLYXBvc1gycHd1SDQ4b1I3cEUvWS8rSDh4RGVqZ1VOTm14MjFWeXNjODVWR1M5M01ZNlozNm42VzlCenk5UktmbFJaQXpLRkZTYlYyYnhrRkhadURteUkyUDhtWnkzTWxqczFESnZjWWYiLCJ3cmFwX3R5cGUiOiJyc2FfMzA3MiIsImttc19lbmRwb2ludCI6ImFjY21oc20ubWFuYWdlZGhzbS5henVyZS5uZXQiLCJhdHRlc3Rlcl9lbmRwb2ludCI6InNoYXJlZGV1czIuZXVzMi5hdHRlc3QuYXp1cmUubmV0In0=")
+            f.write("eyJraWQiOiJ0ZXN0a2V5MDAwIiwid3JhcHBlZF9kYXRhIjoiUEplWGhUMVhEQ0FnYUJBUkdGSTF6SXpveG9KMTVxUHhGcDNRaTU0QWl4Wmw1cWN1V1VCZWFRVGxqVm1DdkZJNEh4OUR3TW1QQlJvbjFIUlo3MTl0d3Z6M0NaYThCdHFMdkY3NVB2VFZZWTgyTjNCN25HYndxTkNXUWM3bkc5MXM0RHNOZkU5c2pZbThSbUhNUVJJeGFlTHVJUUNiOEV3MmNJTTBaK3dBZFJxQlhtOHJCcFd3ci8rSzMrNk51ZW9qZCtqR0FudEhuRnBuRmF1WGZpTXA2RkQyVnN6MEMxQ0VpbWtZalRqTjFCbmkvQVViYUgrS2kxR3VoQzRwaE92cUpTMG9RamplMWFHWUFTRHROZU9oeGw5L0ZCSENPQXV6Yk4xeFpQbUowMTdTTnp2b0taVlBPTGloVWxNcWhQU3I3OW5CV25yM0FRU2NmaXN0R3BUVzFaMGVhTDBBcWhtSC9mb1BWYkI3aFhqMHZrOVY3eVl0dTlJQWhWVVhVMU8rMjdQQUdVcllSa1pSRmtmQzdRcTJLVkQza1d3aXhRL1M5c2RnZ1NUTTlFYU41SEtmdGlpQWs4aDVYTC9jTUs0NU5QU255TFRYZTRmWVMvT0lWdlRpcFBXU2luQkRDc2hISDRxaGNyUFJPTW03Tlp6S0JzdkNMVHVvL0lBQWlWS2giLCJ3cmFwX3R5cGUiOiJyc2FfMzA3MiIsImttc19lbmRwb2ludCI6ImFjY21oc20ubWFuYWdlZGhzbS5henVyZS5uZXQiLCJhdHRlc3Rlcl9lbmRwb2ludCI6InNoYXJlZGV1czIuZXVzMi5hdHRlc3QuYXp1cmUubmV0In0=")
 
     output = subprocess.run("./unwrap.sh wrapped plaintext", stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, input="", shell=True)
     return output
@@ -21,9 +21,9 @@ def grpc_ready_test():
 
 def grpc_snp_report():
     output = create_grpc_test_files()
-    #if output.returncode == 0:
-        #output = subprocess.run("grpcurl -v -plaintext -d '{\"reportDataHexString\":\"\"}' 127.0.0.1:50000  keyprovider.KeyProviderService.GetReport", stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, input="", shell=True)
-    return output.stdout #if output.returncode == 0 else output.stderr
+    if output.returncode == 0:
+        output = subprocess.run("grpcurl -v -plaintext -d '{\"reportDataHexString\":\"\"}' 127.0.0.1:50000  keyprovider.KeyProviderService.GetReport", stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, input="", shell=True)
+    return output
 
 def grpc_key_release():
     output = create_grpc_test_files()

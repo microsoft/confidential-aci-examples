59f12fd8d6b170230288da5c93305bcb055330a8
Heather Garvison <hgarvison@yahoo.com>
2023-11-11 03:34:28 +0000

    wrapped file

--- examples/key_release/unwrap.sh
+++ examples/key_release/unwrap.sh
@@ -22,7 +22,7 @@ fi
 
 AAA=`printf aasp | base64 -w0`
 echo ${infile}
-cat ${infile}
+echo `cat ${infile}`
 ANNO=`cat ${infile}`
 echo ANNO=${ANNO}
 REQ=`echo "{\"op\":\"keyunwrap\",\"keywrapparams\":{},\"keyunwrapparams\":{\"dc\":{\"Parameters\":{\"attestation-agent\":[\"${AAA}\"]}},\"annotation\":\"${ANNO}\"}}" | base64 -w0`

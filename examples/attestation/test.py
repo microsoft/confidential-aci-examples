d2e50f84d3b8ce77536937034197c693957f4049
Dominic Ayre <dominicayre@microsoft.com>
2024-07-10 12:49:08 +0000

    Update tests to use the new interface

--- examples/attestation/test.py
+++ examples/attestation/test.py
@@ -1,11 +1,19 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+import argparse
 import unittest
 import os
 import uuid
 import requests
 
+from c_aci_testing.args.parameters.location import parse_location
+from c_aci_testing.args.parameters.managed_identity import \
+    parse_managed_identity
+from c_aci_testing.args.parameters.registry import parse_registry
+from c_aci_testing.args.parameters.repository import parse_repository
+from c_aci_testing.args.parameters.resource_group import parse_resource_group
+from c_aci_testing.args.parameters.subscription import parse_subscription
 from c_aci_testing.tools.target_run import target_run_ctx
 from c_aci_testing.tools.aci_get_ips import aci_get_ips
 
@@ -17,14 +25,22 @@ except ImportError:
 class AttestationTest(unittest.TestCase):
     def test_attestation(self):
 
-        target_dir = os.path.realpath(os.path.dirname(__file__))
+        parser = argparse.ArgumentParser()
+        parse_subscription(parser)
+        parse_resource_group(parser)
+        parse_registry(parser)
+        parse_repository(parser)
+        parse_location(parser)
+        parse_managed_identity(parser)
+        args, _ = parser.parse_known_args()
+
         id = str(uuid.uuid4())
 
         with target_run_ctx(
-            target=target_dir,
-            name=f"attestation-{id}",
+            target_path=os.path.realpath(os.path.dirname(__file__)),
+            deployment_name=f"attestation-{id}",
             tag=id,
-            follow=False,
+            **vars(args),
         ) as deployment_ids:
             ip_address = aci_get_ips(ids=deployment_ids[0])
 
